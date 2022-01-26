4abfb8ea1ca9da1f8e5b3e92945006bbe148708f
G8tekeeper <G8tekeeper@gmail.com>
2022-01-27 02:05:38 +0900

    Deleted the middleware auth:api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Auth\RegisterController;
 
@@ -17,22 +18,17 @@ use App\Http\Controllers\Auth\RegisterController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request, User $user) {
+//     return $request->user();
+// });
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('/registURL', [RegisterController::class, 'registURL']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
 Route::group(['middleware' => 'api'], function () {
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/me', [AuthController::class, 'me']);
     Route::post('/registUser', [RegisterController::class, 'registUser']);
     Route::post('/registURL', [RegisterController::class, 'registURL']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
