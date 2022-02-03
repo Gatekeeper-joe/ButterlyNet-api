07b32e69db6c571d7b46fd0786d871a48a37b948
G8tekeeper <G8tekeeper@gmail.com>
2022-02-03 20:00:46 +0900

    First commit.

---
+++ app/Http/Controllers/GetController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Site;
+use Log;
+
+class GetController extends Controller
+{
+    public function getUpdated(Request $request, Site $site)
+    {
+        $uid = $request->uid;
+        $updated_page_data = $site->where('user_id', $uid)->where('update_flag', 1)->get(['host', 'url', 'last_updated_at']);
+        return $updated_page_data;
+    }
+
+    public function toObject($array)
+    {
+        $obj = new \stdClass;
+        foreach ($array as $k => $v) {
+            $obj->{$k} = $v;
+        }
+
+        return $obj;
+    }
+}
