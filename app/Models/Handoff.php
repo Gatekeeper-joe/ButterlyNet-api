aa74aa243a07572d70b5aa8e17435e2e7286f923
G8tekeeper <G8tekeeper@gmail.com>
2022-02-21 15:20:17 +0900

    First commit.

---
+++ app/Models/Handoff.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Log;
+
+class Handoff extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['group_id', 'subject', 'body', 'status', 'responder', 'updater'];
+
+    //This is named saveData because of duplicating with Model::save().
+    public function saveData($data)
+    {
+        $key = array_keys($data);
+        if ($key[0] === 'newItem') {
+            $gid = $data['newItem']['group_id'];
+            $subject = $data['newItem']['subject'];
+            $body = $data['newItem']['body'];
+            $status = $data['newItem']['status'];
+
+            $this->fill(['group_id' => $gid, 'subject' => $subject, 'body' => $body, 'status' => $status]);
+            $this->save();
+
+            $record = $this->where('group_id', $gid)->get();
+            return $record;
+        } elseif ($key[0] === 'obj') {
+            $gid = $data['obj']['gid'];
+            $id = $data['obj']['id'];
+            $subject = $data['obj']['subject'];
+            $status = $data['obj']['status'];
+
+            $this->where('id', $id)->update(['subject' => $subject, 'status' => $status]);
+            $records = $this->where('group_id', $gid)->get();
+            return $records;
+        }
+    }
+
+    public function get($gid)
+    {
+        return $this->where('group_id', $gid)->get();
+    }
+}
