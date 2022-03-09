fdc353aceeac39738e51755f3f86b2ddbca93f7b
G8tekeeper <G8tekeeper@gmail.com>
2022-03-09 15:43:48 +0900

    Added [use Illuminate\Support\Facades\Hash;].

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use App\Rules\halfWidth;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 use Log;
 
 class AuthController extends Controller
