e5334f5ed7fa7fe132e1f98c3fa6a7fb93ea6d94
G8tekeeper <G8tekeeper@gmail.com>
2022-03-15 10:05:35 +0900

    Added /createGroup, /joinGroup.

--- routes/api.php
+++ routes/api.php
@@ -30,14 +30,16 @@ Auth::routes();
 Route::group(['middleware' => 'api'], function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/registUser', [RegisterController::class, 'registUser']);
     Route::post('/registURL', [RegisterController::class, 'registURL']);
-    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/getUpdated', [GetController::class, 'getUpdated']);
-    Route::post('/getRecord', [GetController::class, 'getRecord']);
+    Route::get('/getRecord', [GetController::class, 'getRecord']);
     Route::post('/updateFlag', [UpdateController::class, 'updateFlag']);
     Route::post('/create', [UpdateController::class, 'save']);
     Route::post('/update', [UpdateController::class, 'save']);
     Route::post('/delete', [deleteController::class, 'execute']);
+    Route::post('/createGroup', [UpdateController::class, 'createGroup']);
+    Route::post('/joinGroup', [UpdateController::class, 'joinGroup']);
 });
