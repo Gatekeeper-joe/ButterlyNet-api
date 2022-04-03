ec4a74f4c0d432ed9fee7d6fe7c772dc5bf1dc4d
G8tekeeper <G8tekeeper@gmail.com>
2022-04-03 11:42:13 +0900

    Deleted not needed lines.

--- app/Http/Controllers/GetController.php
+++ app/Http/Controllers/GetController.php
@@ -2,30 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers;
-use Illuminate\Http\Request;
 use App\Models\Site;
 use App\Models\Handoff;
-use Carbon\Carbon;
-use Log;
 
 class GetController extends Controller
 {
     public function getUpdated(Site $site)
     {
         $updated_page_data = $site->where('update_flag', 1)->get(['host', 'url', 'last_updated_at']);
-        Log::info($updated_page_data);
         return $updated_page_data;
-    }
-
-    public function toObject($array)
-    {
-        $obj = new \stdClass;
-        foreach ($array as $k => $v) {
-            $obj->{$k} = $v;
-        }
-
-        return $obj;
     }
 
     public function getRecord(Handoff $handoff)
